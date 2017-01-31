e8691523c511d1881e75aa6a474ce413622abff1
sozemego <piknypiec@gmail.com>
2017-01-31 22:41:25 +0100

    wording change in datadisplay

--- app/components/datadisplay/datadisplay.js
+++ app/components/datadisplay/datadisplay.js
@@ -29,10 +29,9 @@ export default class DataDisplay extends React.Component {
 
 		return(
 			<div className = "container">
-				<p className = "lead text-center">This grid displays all columns found in your file.</p>
-				<p className = "text-center">Selecting those, you will define scales used in your experiment.</p>
-				<p className = "text-center">It is important to properly define your scales, otherwise a wrong statistical test will be used, which is not good for your professor&#39;s blood pressure.</p>
-				<p className = "text-center">Remember to define variables like gender or education as scales too, this will allow you to make additional statistical comparisons.</p>
+				<p className = "lead text-center">This grid displays all columns (items) found in your file.</p>
+				<p className = "text-center">Select items belonging to your (sub)scales and create them.</p>
+				<p className = "text-center">Don&#39;t worry, if you miss one, you will be able to come back and add them.</p>
 				<Separator />
 				<div className = "row">
 					{cells}
