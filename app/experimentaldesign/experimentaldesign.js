e11e93c1c35088a5ae2b7f7f06c23a37db045315
sozemego <piknypiec@gmail.com>
2017-02-05 13:11:25 +0100

    Scale names can only be unique

--- app/experimentaldesign/experimentaldesign.js
+++ app/experimentaldesign/experimentaldesign.js
@@ -8,12 +8,35 @@ export default class ExperimentalDesign {
 		this.scales = [];
 		this.getScales = this.getScales.bind(this);
 		this.getScale = this.getScale.bind(this);
+		this.exists = this.exists.bind(this);
 	}
 
+	/**
+		Returns true if adding the scale was successful, false otherwise.
+		Adding the scale is unsuccessful if the scale is undefined or
+		a scale with the same name already exists.
+	*/
 	addScale(scale) {
 		if(scale) {
-			this.scales.push(scale);
+			if(!this.exists(scale.name)) {
+				this.scales.push(scale);
+				return true;
+			}
 		}
+		return false;
+	}
+
+	/**
+		Returns true if a scale with given name already exists,
+		false otherwise.
+	*/
+	exists(name) {
+		for(var i = 0; i < this.scales.length; i++) {
+			if(this.scales[i].name === name) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	getScales() {
