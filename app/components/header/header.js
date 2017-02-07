00ae270336d3793f8eea4a91886e20ab0a162e0c
sozemego <piknypiec@gmail.com>
2017-02-07 20:07:06 +0100

    Comments and formatting

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,22 +1,23 @@
 import React from "react";
 
 import Logo from "../../components/logo/logo";
-//import styles from "./header.css";
 
+/**
+	The header, which contains the logo.
+*/
 export default class Header extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return(
-      <div className = "container_fluid">
-        <div className = "row">
-          <Logo logo = "img/logo.png"/>
-        </div>
-      </div>
-    )
-  }
-
+	render() {
+		return(
+			<div className = "container_fluid">
+				<div className = "row">
+					<Logo logo = "img/logo.png"/>
+				</div>
+			</div>
+		);
+	}
 }
