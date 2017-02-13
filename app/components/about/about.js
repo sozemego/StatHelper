11038861ee1ea7627ae26e3c5fa093e2cafb0551
sozemego <piknypiec@gmail.com>
2017-02-13 18:51:39 +0100

    Formatting change

--- app/components/about/about.js
+++ app/components/about/about.js
@@ -4,12 +4,12 @@ import styles from "./about.css";
 
 export default class About extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return(
+	render() {
+		return(
       <div className = "container_fluid">
         <div className = {styles.about_container + " " + styles.about_text}>
           <p className = {"lead text-center " + styles.top_element}>Are you a psychology student who has a little trouble handling their statistics & methodology section of your paper?</p>
@@ -17,7 +17,7 @@ export default class About extends React.Component {
           <p className = "lead text-center">Want some pretty charts sprinkled throughout your paper?</p>
         </div>
       </div>
-    )
-  }
+		);
+	}
 
 }
