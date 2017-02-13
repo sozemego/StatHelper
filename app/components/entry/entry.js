ca7874924858481f35509ae6010545b1f7fb2cd2
sozemego <piknypiec@gmail.com>
2017-02-13 19:31:21 +0100

    Formatting change

--- app/components/entry/entry.js
+++ app/components/entry/entry.js
@@ -2,17 +2,16 @@ import React from "react";
 
 export default class Entry extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return(
+	render() {
+		return(
       <a href = "/app">
         <img src = "img/entry.png" className = "center-block img-responsive"></img>
       </a>
-    )
-  }
-
+		);
+	}
 
 }
