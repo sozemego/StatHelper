03d9ae472b6cc980ce3af1d2f3828d2013c5a526
sozemego <piknypiec@gmail.com>
2017-07-12 18:33:17 +0200

    Added missing file to git

---
+++ src/common/component/SelectableElementCollectionComponent.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {Chip} from "material-ui";
+
+const container = {
+    display: "flex",
+    flexWrap: "wrap",
+    marginTop: "6px"
+};
+
+const selectedElement = {
+    backgroundColor: "orange"
+};
+
+export default class SelectableElementCollectionComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    _isElementSelected = (index) => {
+        return this.props.selectedElementIndex === index;
+    };
+
+    render() {
+        return (
+            <div style={container}>
+                {this.props.elements.map((element, index) => {
+                    return <Chip
+                        onTouchTap={() => this.props.selectElement(index)}
+                        key={index}
+                        style={this._isElementSelected(index) ? selectedElement : {}}
+                    >
+                        {element}
+                    </Chip>
+                })}
+            </div>
+        )
+    }
+}
