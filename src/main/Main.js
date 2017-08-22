daa90794b871574bcfa2708a32766cfa72c9e6fd
Sozemego <piknypiec@gmail.com>
2017-08-22 16:33:19 +0200

    Added selection to tabs to speed up development

--- src/main/Main.js
+++ src/main/Main.js
@@ -3,6 +3,7 @@ import {Tab, Tabs} from "material-ui";
 import DataContainer from "../file-processing/component/DataContainer";
 import ScalesContainer from "../scales/components/ScalesContainer";
 import ExperimentalDesignContainer from "../experimental-design/components/ExperimentalDesignContainer";
+import StatsContainer from "../stats/components/StatsContainer";
 
 export default class Main extends React.Component {
 
@@ -12,15 +13,18 @@ export default class Main extends React.Component {
 
     render() {
         return(
-            <Tabs>
-                <Tab label="Data">
+            <Tabs value={4}>
+                <Tab label="Data" value={1}>
                     <DataContainer />
                 </Tab>
-                <Tab label="Scales">
+                <Tab label="Scales" value={2}>
                     <ScalesContainer />
                 </Tab>
-                <Tab label="Experimental design">
+                <Tab label="Experimental design" value={3}>
                     <ExperimentalDesignContainer />
+                </Tab>
+                <Tab label="Stats" value={4}>
+                    <StatsContainer />
                 </Tab>
             </Tabs>
         )
