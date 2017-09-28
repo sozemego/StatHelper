a2c76ba6a2aaf0aa4877d96ef673c0f8ce5e8e03
sozemego <piknypiec@gmail.com>
2017-09-28 07:56:52 +0200

    More tests

--- test/stats/test-runner/test-runner.test.js
+++ test/stats/test-runner/test-runner.test.js
@@ -1,22 +1,8 @@
 import {runTest} from '../../../src/stats/test-runner/test-runner';
 import {CORRELATION} from '../../../src/experimental-design/model/test';
 
-const data = [
-	['Gender', 'Age', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-	[1, 25, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 16, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[2, 18, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 22, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[2, 17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[2, 18, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-];
-
 const scales = [
-	{name: 'Scale1', result: [1, 2, 2, 3, 4, 5, 1, 2, 3, 4, 5]},
+	{name: 'Scale1', result: [1, 2, 2, 3, 4, 5, 1, 2, 3, 4, 5, 4, 4, 4, 4, 5, 5, 5, 1, 1, 1, 2, 2, 2]},
 	{name: 'Scale2', result: [5, 2, 2, 1, 1, 1, 1, 12, 3, 4, 5]},
 	{name: 'Scale3', result: [1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5]},
 	{name: 'Scale4', result: [1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5, 5]}
@@ -31,10 +17,27 @@ describe('test runner', () => {
 		const test = {type: CORRELATION, name: 'Test', scales: [{}]};
 		expect(() => runTest(test)).toThrow();
 	});
-	it('should calculate correlation tests for two scales with normally distributed samples', () => {
+	it('should calculate pearson correlation test for two scales with normally distributed samples', () => {
 		const test = {type: CORRELATION, name: 'Test', scales: scales.slice().splice(2)};
 		const result = runTest(test);
 		expect(result.length).toBe(1);
 		expect(result[0].testName).toBe('pearson');
+	});
+	it('should calculate spearman correlation test for two scales with non-normally distributed samples', () => {
+		const test = {type: CORRELATION, name: 'Test', scales: scales.slice().splice(0, 2)};
+		const result = runTest(test);
+		expect(result.length).toBe(1);
+		expect(result[0].testName).toBe('spearman');
+	});
+	it('should calculate spearman correlation for one scale with normal distribution and another with non-normal', () => {
+		const test = {type: CORRELATION, name: 'Test', scales: [scales[0], scales[3]]};
+		const result = runTest(test);
+		expect(result.length).toBe(1);
+		expect(result[0].testName).toBe('spearman');
+	});
+	it('should perform 3 tests for 3 scales, one for each scale pair', () => {
+		const test = {type: CORRELATION, name: 'Test', scales: [scales[0], scales[1], scales[2]]};
+		const result = runTest(test);
+		expect(result.length).toBe(3);
 	});
 });
