5667a5ccc1162d8de487225a8455d0cafbe0eefc
sozemego <piknypiec@gmail.com>
2017-10-04 08:41:20 +0200

    Improved tests, some implementation of descriptives

--- src/stats/test-runner/descriptive-statistics.js
+++ src/stats/test-runner/descriptive-statistics.js
@@ -7,7 +7,67 @@
  * All scales get a measure of sample size.
  * @param scale
  */
+import {NOMINAL, ORDINAL, RATIO} from '../../scales/model/scale';
+
 export const getDescriptives = scale => {
+	const handler = scaleHandlers[scale.measurementLevel];
+	if (!handler) {
+		throw new Error('Invalid scale type');
+	}
+	return handler(scale);
+};
 
+const nominalScaleHandler = scale => {
+	const {result} = scale;
+	const frequencies = [];
+	for (let i = 0; i < result.length; i++) {
+		frequencies.push({});
+	}
 
+	createFrequencyCount(result, frequencies);
+
+	return {
+		frequencies
+	};
+};
+
+/**
+ * Counts all occurences of all values in an result array,
+ * and for each occurence, appends a
+ * @param results
+ * @param frequencyMap
+ */
+const createFrequencyCount = (results, frequencyMap) => {
+	const map = {};
+
+	for (let i = 0; i < results.length; i++) {
+		const result = results[i];
+		if (!map[result]) {
+			map[result] = 1;
+		} else {
+			map[result] = ++map[result];
+		}
+	}
+
+	let i = 0;
+	for (const key in map) {
+		frequencyMap[i].count = map[key];
+		frequencyMap[i].value = key;
+		i++;
+	}
+	frequencyMap.sort((a, b) => b.count - a.count);
+};
+
+const ordinalScaleHandler = scale => {
+
+};
+
+const ratioScaleHandler = scale => {
+
+};
+
+const scaleHandlers = {
+	[NOMINAL]: nominalScaleHandler,
+	[ORDINAL]: ordinalScaleHandler,
+	[RATIO]: ratioScaleHandler
 };
--- test/stats/test-runner/descriptive-statistics.test.js
+++ test/stats/test-runner/descriptive-statistics.test.js
@@ -1,6 +1,17 @@
 import {NOMINAL, ORDINAL} from '../../../src/scales/model/scale';
 import {getDescriptives} from '../../../src/stats/test-runner/descriptive-statistics';
 
+describe('descriptive statistics for invalid scale', () => {
+	it('should throw error for an unknown measurement level', () => {
+		const scale = {
+			name: 'scale',
+			measurementLevel: 'level',
+			result: ['a', 'b', 'c']
+		};
+		expect(() => getDescriptives(scale)).toThrow();
+	});
+});
+
 describe('descriptive statistics for nominal scale', () => {
 	it('should return items by frequency, sorted', () => {
 		const scale = {
@@ -9,12 +20,16 @@ describe('descriptive statistics for nominal scale', () => {
 			result: ['cool', 'great', 'fine', 'amazing', 'cool', 'great', 'great', 'fine', 'amazing']
 		};
 		const descriptives = getDescriptives(scale);
-		const {frequencyMap, sampleSize} = descriptives;
-		expect(frequencyMap).toBeDefined();
-		expect(frequencyMap.find(value => value === 'cool').count).toBe(2);
-		expect(frequencyMap.find(value => value === 'great').count).toBe(3);
-		expect(frequencyMap.find(value => value === 'amazing').count).toBe(2);
-		expect(frequencyMap.find(value => value === 'fine').count).toBe(2);
+		const {frequencies, sampleSize} = descriptives;
+		expect(frequencies).toBeDefined();
+		expect(frequencies.find(element => element.value === 'cool').count).toBe(2);
+		expect(frequencies.find(element => element.value === 'great').count).toBe(3);
+		expect(frequencies.find(element => element.value === 'amazing').count).toBe(2);
+		expect(frequencies.find(element => element.value === 'fine').count).toBe(2);
+		expect(frequencies.findIndex(element => element.value === 'great')).toBe(0);
+		expect(frequencies.findIndex(element => element.value === 'cool')).toBe(1);
+		expect(frequencies.findIndex(element => element.value === 'fine')).toBe(2);
+		expect(frequencies.findIndex(element => element.value === 'amazing')).toBe(3);
 		expect(sampleSize).toBe(9);
 	});
 	it('should return items by percentage, sorted', () => {
@@ -24,113 +39,117 @@ describe('descriptive statistics for nominal scale', () => {
 			result: ['cool', 'great', 'fine', 'amazing', 'cool', 'great', 'great', 'fine', 'amazing']
 		};
 		const descriptives = getDescriptives(scale);
-		const {frequencyMap, sampleSize} = descriptives;
-		expect(frequencyMap).toBeDefined();
-		expect(frequencyMap.find(value => value === 'cool').percent).toBe(22.2);
-		expect(frequencyMap.find(value => value === 'great').percent).toBe(33.3);
-		expect(frequencyMap.find(value => value === 'amazing').percent).toBe(22.2);
-		expect(frequencyMap.find(value => value === 'fine').percent).toBe(22.2);
+		const {frequencies, sampleSize} = descriptives;
+		expect(frequencies).toBeDefined();
+		expect(frequencies.find(element => element.value === 'cool').percent).toBe(22.2);
+		expect(frequencies.find(element => element.value === 'great').percent).toBe(33.3);
+		expect(frequencies.find(element => element.value === 'amazing').percent).toBe(22.2);
+		expect(frequencies.find(element => element.value === 'fine').percent).toBe(22.2);
+		expect(frequencies.findIndex(element => element.value === 'amazing')).toBe(0);
+		expect(frequencies.findIndex(element => element.value === 'cool')).toBe(1);
+		expect(frequencies.findIndex(element => element.value === 'great')).toBe(2);
+		expect(frequencies.findIndex(element => element.value === 'fine')).toBe(3);
 		expect(sampleSize).toBe(9);
 	});
 });
 
-describe('descriptive statistics for ordinal scale', () => {
-	it('should return items by frequency, sorted', () => {
-		const scale = {
-			name: 'scale',
-			measurementLevel: ORDINAL,
-			result: [1, 2, 6, 4, 1, 2, 8, 12, 1]
-		};
-		const descriptives = getDescriptives(scale);
-		const {frequencyMap, sampleSize} = descriptives;
-		expect(frequencyMap).toBeDefined();
-		expect(frequencyMap.find(value => value === 1).count).toBe(2);
-		expect(frequencyMap.find(value => value === 2).count).toBe(2);
-		expect(frequencyMap.find(value => value === 6).count).toBe(1);
-		expect(frequencyMap.find(value => value === 8).count).toBe(1);
-		expect(frequencyMap.find(value => value === 12).count).toBe(1);
-		expect(frequencyMap.findIndex(value => value === 1)).toBe(0);
-		expect(frequencyMap.findIndex(value => value === 2)).toBe(1);
-		expect(frequencyMap.findIndex(value => value === 6)).toBe(2);
-		expect(frequencyMap.findIndex(value => value === 8)).toBe(3);
-		expect(frequencyMap.findIndex(value => value === 12)).toBe(4);
-		expect(sampleSize).toBe(9);
-	});
-	it('should return items with percentage frequency, sorted', () => {
-		const scale = {
-			name: 'scale',
-			measurementLevel: ORDINAL,
-			result: [1, 2, 6, 4, 1, 2, 8, 12, 1, 1, 5, 5, 12]
-		};
-		const descriptives = getDescriptives(scale);
-		const {frequencyMap, sampleSize} = descriptives;
-		expect(frequencyMap).toBeDefined();
-		expect(frequencyMap.find(value => value === 1).percent).toBe(30.8);
-		expect(frequencyMap.find(value => value === 2).percent).toBe(15.4);
-		expect(frequencyMap.find(value => value === 4).percent).toBe(7.7);
-		expect(frequencyMap.find(value => value === 5).percent).toBe(15.4);
-		expect(frequencyMap.find(value => value === 6).percent).toBe(7.7);
-		expect(frequencyMap.find(value => value === 8).percent).toBe(7.7);
-		expect(frequencyMap.find(value => value === 12).percent).toBe(15.4);
-		expect(frequencyMap.findIndex(value => value === 1)).toBe(0);
-		expect(frequencyMap.findIndex(value => value === 2)).toBe(1);
-		expect(frequencyMap.findIndex(value => value === 5)).toBe(2);
-		expect(frequencyMap.findIndex(value => value === 4)).toBe(3);
-		expect(frequencyMap.findIndex(value => value === 6)).toBe(4);
-		expect(frequencyMap.findIndex(value => value === 8)).toBe(4);
-		expect(frequencyMap.findIndex(value => value === 12)).toBe(15.4);
-		expect(sampleSize).toBe(13);
-	});
-	it('should return items by frequency, sorted, v2', () => {
-		const scale = {
-			name: 'scale',
-			measurementLevel: ORDINAL,
-			result: [1, 2, 6, 4, 1, 2, 8, 12, 1, 14, 14, 14, 12, 15, 1, 1, 1, 0, 2, 4, 4, 4, 4, 4, 0]
-		};
-		const descriptives = getDescriptives(scale);
-		const {frequencyMap, sampleSize} = descriptives;
-		expect(frequencyMap).toBeDefined();
-		expect(frequencyMap.find(value => value === 0).count).toBe(2);
-		expect(frequencyMap.find(value => value === 1).count).toBe(6);
-		expect(frequencyMap.find(value => value === 12).count).toBe(2);
-		expect(frequencyMap.find(value => value === 14).count).toBe(3);
-		expect(frequencyMap.find(value => value === 15).count).toBe(1);
-		expect(frequencyMap.find(value => value === 2).count).toBe(3);
-		expect(frequencyMap.find(value => value === 4).count).toBe(6);
-		expect(frequencyMap.find(value => value === 6).count).toBe(1);
-		expect(frequencyMap.find(value => value === 8).count).toBe(1);
-		expect(frequencyMap.findIndex(value => value === 1)).toBe(0);
-		expect(frequencyMap.findIndex(value => value === 4)).toBe(1);
-		expect(frequencyMap.findIndex(value => value === 14)).toBe(2);
-		expect(frequencyMap.findIndex(value => value === 2)).toBe(3);
-		expect(frequencyMap.findIndex(value => value === 0)).toBe(4);
-		expect(frequencyMap.findIndex(value => value === 12)).toBe(5);
-		expect(frequencyMap.findIndex(value => value === 15)).toBe(6);
-		expect(frequencyMap.findIndex(value => value === 6)).toBe(7);
-		expect(frequencyMap.findIndex(value => value === 8)).toBe(8);
-		expect(sampleSize).toBe(9);
-	});
-	it('should return correct mode', () => {
-		const scale = {
-			name: 'scale',
-			measurementLevel: ORDINAL,
-			result: [1, 2, 6, 4, 1, 2, 8, 12, 1]
-		};
-		const descriptives = getDescriptives(scale);
-		const {modes} = descriptives;
-		expect(modes).toBeDefined();
-		expect(modes.length).toBe(1);
-		expect(modes[0]).toBe(1);
-	});
-	it('should return correct median', () => {
-		const scale = {
-			name: 'scale',
-			measurementLevel: ORDINAL,
-			result: [1, 2, 6, 4, 1, 2, 8, 12, 1]
-		};
-		const descriptives = getDescriptives(scale);
-		const {median} = descriptives;
-		expect(median).toBeDefined();
-		expect(median).toBe(2);
-	});
-});
+// describe('descriptive statistics for ordinal scale', () => {
+// 	it('should return items by frequency, sorted', () => {
+// 		const scale = {
+// 			name: 'scale',
+// 			measurementLevel: ORDINAL,
+// 			result: [1, 2, 6, 4, 1, 2, 8, 12, 1]
+// 		};
+// 		const descriptives = getDescriptives(scale);
+// 		const {frequencies, sampleSize} = descriptives;
+// 		expect(frequencies).toBeDefined();
+// 		expect(frequencies.find(element => element.value === 1).count).toBe(2);
+// 		expect(frequencies.find(element => element.value === 2).count).toBe(2);
+// 		expect(frequencies.find(element => element.value === 6).count).toBe(1);
+// 		expect(frequencies.find(element => element.value === 8).count).toBe(1);
+// 		expect(frequencies.find(element => element.value === 12).count).toBe(1);
+// 		expect(frequencies.findIndex(element => element.value === 1)).toBe(0);
+// 		expect(frequencies.findIndex(element => element.value === 2)).toBe(1);
+// 		expect(frequencies.findIndex(element => element.value === 6)).toBe(2);
+// 		expect(frequencies.findIndex(element => element.value === 8)).toBe(3);
+// 		expect(frequencies.findIndex(element => element.value === 12)).toBe(4);
+// 		expect(sampleSize).toBe(9);
+// 	});
+// 	it('should return items with percentage frequency, sorted', () => {
+// 		const scale = {
+// 			name: 'scale',
+// 			measurementLevel: ORDINAL,
+// 			result: [1, 2, 6, 4, 1, 2, 8, 12, 1, 1, 5, 5, 12]
+// 		};
+// 		const descriptives = getDescriptives(scale);
+// 		const {frequencies, sampleSize} = descriptives;
+// 		expect(frequencies).toBeDefined();
+// 		expect(frequencies.find(element => element.value === 1).percent).toBe(30.8);
+// 		expect(frequencies.find(element => element.value === 2).percent).toBe(15.4);
+// 		expect(frequencies.find(element => element.value === 4).percent).toBe(7.7);
+// 		expect(frequencies.find(element => element.value === 5).percent).toBe(15.4);
+// 		expect(frequencies.find(element => element.value === 6).percent).toBe(7.7);
+// 		expect(frequencies.find(element => element.value === 8).percent).toBe(7.7);
+// 		expect(frequencies.find(element => element.value === 12).percent).toBe(15.4);
+// 		expect(frequencies.findIndex(element => element.value === 1)).toBe(0);
+// 		expect(frequencies.findIndex(element => element.value === 2)).toBe(1);
+// 		expect(frequencies.findIndex(element => element.value === 12)).toBe(2);
+// 		expect(frequencies.findIndex(element => element.value === 5)).toBe(3);
+// 		expect(frequencies.findIndex(element => element.value === 4)).toBe(4);
+// 		expect(frequencies.findIndex(element => element.value === 6)).toBe(5);
+// 		expect(frequencies.findIndex(element => element.value === 8)).toBe(6);
+// 		expect(sampleSize).toBe(13);
+// 	});
+// 	it('should return items by frequency, sorted, v2', () => {
+// 		const scale = {
+// 			name: 'scale',
+// 			measurementLevel: ORDINAL,
+// 			result: [1, 2, 6, 4, 1, 2, 8, 12, 1, 14, 14, 14, 12, 15, 1, 1, 1, 0, 2, 4, 4, 4, 4, 4, 0]
+// 		};
+// 		const descriptives = getDescriptives(scale);
+// 		const {frequencies, sampleSize} = descriptives;
+// 		expect(frequencies).toBeDefined();
+// 		expect(frequencies.find(element => element.value === 0).count).toBe(2);
+// 		expect(frequencies.find(element => element.value === 1).count).toBe(6);
+// 		expect(frequencies.find(element => element.value === 12).count).toBe(2);
+// 		expect(frequencies.find(element => element.value === 14).count).toBe(3);
+// 		expect(frequencies.find(element => element.value === 15).count).toBe(1);
+// 		expect(frequencies.find(element => element.value === 2).count).toBe(3);
+// 		expect(frequencies.find(element => element.value === 4).count).toBe(6);
+// 		expect(frequencies.find(element => element.value === 6).count).toBe(1);
+// 		expect(frequencies.find(element => element.value === 8).count).toBe(1);
+// 		expect(frequencies.findIndex(element => element.value === 1)).toBe(0);
+// 		expect(frequencies.findIndex(element => element.value === 4)).toBe(1);
+// 		expect(frequencies.findIndex(element => element.value === 14)).toBe(2);
+// 		expect(frequencies.findIndex(element => element.value === 2)).toBe(3);
+// 		expect(frequencies.findIndex(element => element.value === 0)).toBe(4);
+// 		expect(frequencies.findIndex(element => element.value === 12)).toBe(5);
+// 		expect(frequencies.findIndex(element => element.value === 15)).toBe(6);
+// 		expect(frequencies.findIndex(element => element.value === 6)).toBe(7);
+// 		expect(frequencies.findIndex(element => element.value === 8)).toBe(8);
+// 		expect(sampleSize).toBe(9);
+// 	});
+// 	it('should return correct mode', () => {
+// 		const scale = {
+// 			name: 'scale',
+// 			measurementLevel: ORDINAL,
+// 			result: [1, 2, 6, 4, 1, 2, 8, 12, 1]
+// 		};
+// 		const descriptives = getDescriptives(scale);
+// 		const {modes} = descriptives;
+// 		expect(modes).toBeDefined();
+// 		expect(modes.length).toBe(1);
+// 		expect(modes[0]).toBe(1);
+// 	});
+// 	it('should return correct median', () => {
+// 		const scale = {
+// 			name: 'scale',
+// 			measurementLevel: ORDINAL,
+// 			result: [1, 2, 6, 4, 1, 2, 8, 12, 1]
+// 		};
+// 		const descriptives = getDescriptives(scale);
+// 		const {median} = descriptives;
+// 		expect(median).toBeDefined();
+// 		expect(median).toBe(2);
+// 	});
+// });
