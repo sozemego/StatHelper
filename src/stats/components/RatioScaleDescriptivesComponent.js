9c2d0f4d52677123bc718816112e6f077a25bfb2
sozemego <piknypiec@gmail.com>
2017-10-08 15:59:30 +0200

    Refactor for readability

--- src/stats/components/RatioScaleDescriptivesComponent.js
+++ src/stats/components/RatioScaleDescriptivesComponent.js
@@ -9,26 +9,35 @@ export class RatioScaleDescriptivesComponent extends React.Component {
 	render() {
 		const {descriptive} = this.props;
 		const {results} = descriptive;
+		const {
+			sampleSize,
+			mean,
+			min,
+			max,
+			median,
+			standardDeviation,
+			normality
+		} = results;
 		return (
 			<div style={{display: 'flex', flexDirection: 'column', margin: '4px 25px 4px 25px'}}>
 				<div>Measurement level: {descriptive.measurementLevel}</div>
 				<br/>
 				<div>
-					Sample size of this scale is {results.sampleSize} (n = {results.sampleSize}).
+					Sample size of this scale is {sampleSize} (n = {sampleSize}).
 				</div>
 				<div>
-					On average, your participants scored {results.mean} (m = {results.mean}).
-					Maximum score they achieved was {results.min} and maximum was {results.max}.
+					On average, your participants scored {mean} (m = {mean}).
+					Maximum score they achieved was {min} and maximum was {max}.
 				</div>
 				<div>
-					The median was {results.median} and standard deviation {results.standardDeviation} (SD
-					= {results.standardDeviation}).
+					The median was {median} and standard deviation {standardDeviation} (SD
+					= {standardDeviation}).
 				</div>
 				<div>
-					{results.normality.test} test was used to determine if results of this scale
-					follow a normal distribution. Its significance level was {results.normality.pValue} (p
-					= {results.normality.pValue}).
-					This result shows that results {results.normality.pValue > 0.05 ? 'follow' : 'DO NOT follow'} a
+					{normality.test} test was used to determine if results of this scale
+					follow a normal distribution. Its significance level was {normality.pValue} (p
+					= {normality.pValue}).
+					This result shows that results {normality.pValue > 0.05 ? 'follow' : 'DO NOT follow'} a
 					normal distribution.
 				</div>
 			</div>
