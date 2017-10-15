0a2d434dd346df0e50717c9d86f794d87310c6f2
sozemego <piknypiec@gmail.com>
2017-10-15 22:00:43 +0200

    Refactors

--- src/scales/reducer/scales-reducer.js
+++ src/scales/reducer/scales-reducer.js
@@ -20,21 +20,17 @@ const scales = (state = initialState, action) => {
     case MOUSE_UP:
       return {...state, selectingItems: false};
     case SELECT_ITEMS:
-      selectItems(state, action.scaleId, action.selectedItems);
-      return {...state};
+      return {...state, scaleList: selectItems(state, action.scaleId, action.selectedItems)};
     case ADD_SCALE:
       return {...state, scaleList: [...state.scaleList, action.scale]};
     case SELECT_SCALE:
       return {...state, selectedScale: action.scaleId};
     case SET_SCALE_NAME:
-      setScaleName(state, action.scaleId, action.scaleName);
-      return {...state};
+      return {...state, scaleList: setScaleName(state, action.scaleId, action.scaleName)};
     case SET_MEASUREMENT_LEVEL:
-      setMeasurementLevel(state, action.scaleId, action.measurementLevel);
-      return {...state};
+      return {...state, scaleList: setMeasurementLevel(state, action.scaleId, action.measurementLevel)};
     case SET_SCALE_SCORES:
-      setScores(state, action.scaleId, action.scores);
-      return {...state};
+      return {...state, scaleList: setScores(state, action.scaleId, action.scores)};
     case REMOVE_SCALE:
       return {...state, scaleList: removeScale(state, action.scaleId)};
     default:
@@ -44,28 +40,32 @@ const scales = (state = initialState, action) => {
 
 const setMeasurementLevel = (state, scaleId, measurementLevel) => {
   const scaleIndex = getScaleIndexById(state, scaleId);
-  const scales = getScales(state);
+  const scales = [...getScales(state)];
   scales[scaleIndex] = {...scales[scaleIndex], measurementLevel};
+  return scales;
 };
 
 const selectItems = (state, scaleId, selectedItems) => {
   const scaleIndex = getScaleIndexById(state, scaleId);
-  const scales = getScales(state);
+  const scales = [...getScales(state)];
   scales[scaleIndex] = {...scales[scaleIndex], items: selectedItems};
+  return scales;
 };
 
 const setScaleName = (state, scaleId, name) => {
   const scale = getScaleById(state, scaleId);
   const scaleIndex = getScaleIndexById(state, scaleId);
-  const scales = getScales(state);
+  const scales = [...getScales(state)];
   scales[scaleIndex] = {...scale, name};
+  return scales;
 };
 
 const setScores = (state, scaleId, scores) => {
   const scale = getScaleById(state, scaleId);
   const scaleIndex = getScaleIndexById(state, scaleId);
-  const scales = getScales(state);
+  const scales = [...getScales(state)];
   scales[scaleIndex] = {...scale, scores};
+  return scales;
 };
 
 const removeScale = (state, scaleId) => {
